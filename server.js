8df42d1efd57922e1afe94c4dc8f662be4144fee
Bretbret18 <bret.sperry@gmail.com>
2021-02-24 18:41:40 -0500

    all candidates, single, and delete success

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 // require express.js mod
 const express = require('express');
+// require sqlite3 .. sets execution mode to verbose -> to produce messages in terminal
+// regarding runtime state. Can help explain what SQLite app is doing.
+const sqlite3 = require('sqlite3').verbose();
 
 // PORT designation / app expression
 const PORT = process.env.PORT || 3001;
@@ -9,11 +12,78 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// app.get (root of directory) response JSON ('What shows up in localhost: 3001) .. test route
-// app.get('/', (req, res) => {
-//     res.json({
-//         message: 'Hello World'
-//     });
+// Connect to database
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+        return console.log.error(err.message);
+    }
+
+    console.log('Connected to the election database. ');
+});
+
+// GET all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+
+// GET single candidate
+app.get('/api/candidates/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates
+                WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });            
+});
+
+// DELETE a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            return;
+        }
+
+        res.json({
+            message: 'successfully deleted',
+            changes: this.changes
+        });
+    });
+});
+
+// // Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+// // ES5 function, not arrow function, to use this
+// db.run(sql, params, function(err, result) {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result, this.lastID);
 // });
 
 // Default response for any other request(Not Found) Catch all
@@ -22,7 +92,18 @@ app.use((req, res) => {
 });
 
 
-// starts express.js server on port 3001
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// starts express.js server after DB connection
+db.on('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 });
+
+
+
+// app.get (root of directory) response JSON ('What shows up in localhost: 3001) .. test route
+// app.get('/', (req, res) => {
+//     res.json({
+//         message: 'Hello World'
+//     });
+// });
