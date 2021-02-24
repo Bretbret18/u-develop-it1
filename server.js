196c13696560db712f2c8742804a8ba43e39af4c
Bretbret18 <bret.sperry@gmail.com>
2021-02-24 00:22:24 -0500

    get routes/ server connection

---
+++ server.js
@@ -0,0 +1,28 @@
+// require express.js mod
+const express = require('express');
+
+// PORT designation / app expression
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// app.get (root of directory) response JSON ('What shows up in localhost: 3001) .. test route
+// app.get('/', (req, res) => {
+//     res.json({
+//         message: 'Hello World'
+//     });
+// });
+
+// Default response for any other request(Not Found) Catch all
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+
+// starts express.js server on port 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
